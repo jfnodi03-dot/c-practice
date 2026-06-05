ad36e4e239ef82c25082396825b8f9feb90d3457
jfnodi03-dot <jfnodi03@gmail.com>
2026-06-05 23:36:23 +0600

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int fib (int n);
+
+int main(){
+
+printf("fib is:%d\n",fib(9) );
+return 0;
+}
+
+int fib (int n){
+
+    if (n==0){
+        return 0;
+    }
+    if (n==1){
+        return 1;
+    }
+int fibM= fib (n- 2);
+int fibN= fib (n- 1);
+
+    int fib= fibM + fibN;
+    return fib;
+
+}
+
