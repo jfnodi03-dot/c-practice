69dca37f50e96c9d81f2ed474c4425d94bd50a6f
jfnodi03-dot <jfnodi03@gmail.com>
2026-06-30 23:24:38 +0600

    Add reverse array program in reverse.c

---
+++ lecture6/reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("enter the number of elements:");
+    scanf("%d", &n);
+
+     int arr[n];
+     printf("enter %d elements:",n);
+        for(int i=0; i<n;i++){
+        scanf("%d", &arr[i]);
+        }
+     printf("array in reverse order:");
+     for(int i= n-1; i>=0;i--){
+        printf("%d",arr[i]);
+     }
+     printf ("\n");
+return 0;
+
+}
