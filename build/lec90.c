b59d010cf44a47b2800e588fa1439293cc898679
jfnodi03-dot <jfnodi03@gmail.com>
2026-06-30 22:20:59 +0600

    Add C program to reverse an array
    Implement a program to read and print an array in reverse order.

---
+++ build/lec90.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr;
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array in reverse order: ");
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
