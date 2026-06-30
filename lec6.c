b8f1dad35ef415818b9d713836e476f60da489ab
jfnodi03-dot <jfnodi03@gmail.com>
2026-06-30 21:37:47 +0600

    Update lec6.c

--- lec6.c
+++ lec6.c
@@ -17,3 +17,26 @@ printf("maximun num is : %d\n", max);
 return 0;
 
 }
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr;
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array in reverse order: ");
+    for(int i = n - 1; i >= 0; i--) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
