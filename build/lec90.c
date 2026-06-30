63ef4bbeead4110bbf87fe7d77665b9f297a35a1
jfnodi03-dot <jfnodi03@gmail.com>
2026-06-30 22:23:22 +0600

    find maximum of two numbers

--- build/lec90.c
+++ build/lec90.c
@@ -1,21 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+int main (){
+int num1, num2,max;
+int *ptr1 , *ptr2 ;
 
-    int arr;
-    printf("Enter %d elements: ", n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
+printf ("enter the number:");
+scanf ("%d  %d", &num1 , &num2);
 
-    printf("Array in reverse order: ");
-    for(int i = n - 1; i >= 0; i--) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+ptr1 = &num1;
+ptr2 = &num2;
+if(*ptr1 >*ptr2 ){
+    max= *ptr1;
+} else {max= *ptr2;
+}
+printf("maximun num is : %d\n", max);
+return 0;
 
-    return 0;
 }
